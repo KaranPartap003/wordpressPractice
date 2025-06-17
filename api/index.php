adbeae9777f2178039223c33bb6e2e83d13f65d8
Karan Partap <122453969+KaranPartap003@users.noreply.github.com>
2025-06-17 22:09:56 +0530

    Delete api directory

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    echo json_encode(['message' => 'Hello World']);
-} else {
-    http_response_code(405);
-    echo json_encode(['error' => 'Method Not Allowed']);
-}
-?>
